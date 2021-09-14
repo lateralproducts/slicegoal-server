9088eda6508f68545d7a6e715f9137d8a46d58e5
Caleb <caleb.a.schrader@gmail.com>
2021-09-14 16:18:37 +0300

    feat: New Shared Insights Message CAV-259

--- src/insights.js
+++ src/insights.js
@@ -11,6 +11,7 @@ export const typeDefs = `
     insights(area: String): [InsightLink]
     insightLinks(insightid: String): [InsightLink]
     searchinsights(search: String, spaced: Boolean): [Insight]
+    newsharedinsights: Boolean
   }
 
   extend type Mutation {
@@ -95,6 +96,24 @@ export const resolvers = {
             ).toArray()
 
             return insights
+        },
+        newsharedinsights: async (parent, args, { req }) => {
+            if (!req.session.user) throw new Error('Invalid Session')
+            const db = await DbConnection.Get()
+            const Insights = db.collection('insights')
+            const Users = db.collection('users')
+
+            const user = await Users.findOne({
+                _id: ObjectId(getuserid(req.session)),
+            })
+
+            const found = await Insights.findOne({
+                email: user.email,
+                status: 'newshared',
+            })
+
+            if (found) return true
+            else return false
         },
         insightLinks: async (parent, args, { req }) => {
             if (!req.session.user) throw new Error('Invalid Session')
