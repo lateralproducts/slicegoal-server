a71946474bb3b1e7c87e7feeb275aa0dad5dfa1c
Daniel Schrader <daniel@lateralproducts.com>
2024-03-18 13:19:11 +1100

    fix: add lateralproducts email to non-authorised functions

--- src/graphqlserver.js
+++ src/graphqlserver.js
@@ -162,7 +162,7 @@ const graphQLServer = createServer({
 });
 
 // List of query names that can be accessed by unauthenticated users
-const unauthenticatedQueries = ['isLoggedin', 'login', 'googleLogin', 'trackpage', 'resetPassword', 'setPassword'];
+const unauthenticatedQueries = ['isLoggedin', 'login', 'googleLogin', 'trackpage', 'resetPassword', 'setPassword', 'sendlateralproductsemail'];
 
 async function authMiddleWare(resolve, root, args, context, info) {
     if (!unauthenticatedQueries.includes(info.fieldName)){
