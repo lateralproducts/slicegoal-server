f98f491b8b7afc6e7e27549baccda6099170a970
Daniel Schrader <daniel@lateralproducts.com>
2025-07-07 23:40:02 +1000

    fix: try new push notifications

--- src/schedules.js
+++ src/schedules.js
@@ -2,20 +2,155 @@ import { ObjectId } from 'mongodb'
 import DbConnection from './database'
 import { emailGoalNudge, emailRerankNudge, emailFunnel, reSendEmail, emailMessageNudge, emailWeeklySummary } from './emails'
 import { createreport, dailyafternoonemail, dailymorningemail, weeklysummaryemail } from './reporting'
-import { date2str } from '../util/functions'
+import { date2str, daylater, shiftTZ, startOfDay } from '../util/functions'
 import { getunreadmessageusers } from './chat'
 import { pushNotification } from './push'
 
 let schedule = require('node-schedule')
 
-schedule.scheduleJob({ hour: 21, minute: 54}, function() { //15:00 UTC = 2:00am/1:00am, 20:30 UTC = 7:30am/6:30am Sydney/Melbourne time
+const hard_coded_userids = ["6710df4f6ba00c0666607a8d", "5d2adcf120f52b0d7d7faba0"] //a8d is dev, ba0 is prod
+
+schedule.scheduleJob({ hour: 20, minute: 0}, async function() { //15:00 UTC = 2:00am/1:00am, 20:30 UTC = 7:30am/6:30am Sydney/Melbourne time
+    //push notification
+    //if there are tasks scheduled for today, send 1st task in list to the user, and link to day tasks.
+    //if there are no tasks scheduled for today, send a push notification to the user to check in on their past tasks.
+    try {
+        const db = await DbConnection.Get()
+        const Tasks = db.collection('tasks')
+
+        const datetime = new Date() //time set from client argument
+        const tzdatetime = shiftTZ({datetime: datetime, timezoneOffset: -11})
+        const starttime = startOfDay(tzdatetime)
+        const endtime = daylater(tzdatetime) // can retire this later if I want to migrate old DB records.
+
+        let query = {}
+        query.profile = { $in: hard_coded_userids }
+        query.$or = [ //only return if complete not equal to true (or doesn't exist)
+            {complete: null},
+            {complete: false},
+            {complete: {$exists: false}}
+        ]
+
+        query.$and = [ //only return if starttime is between starttime and endtime and snooze is null or snooze is in the past
+            {'starttime': {$gte: starttime}},
+            {'starttime': {$lt: endtime}},
+            {$or: [
+                {snooze: null},
+                {snooze: {$exists: false}},
+                {snooze: {$lt: new Date()}}
+            ]}
+        ]
+
+        const tasks = await Tasks.find(query).toArray()
+        console.log(tasks)
+
+        if (tasks.length > 0) {
+            pushNotification(
+                'fJ7odiluI04AsQVnke5sSz:APA91bHj7xytmfiuBxjm7cvcfADVGgdrs8mhrHPfJvhnVbbj34C0rNrrHHxlBReyRuYYtjO-SczSxYvkdfno5zsvSGuO1UuZkQMMlYOSS4C1I7Urh_YF-1M',
+                'Your tasks for today!',
+                `${tasks[0].title}`,
+                {url: '?rh=tasks&tnav=daytasklist'}
+            )
+        }
+        else {
+            pushNotification(
+                'fJ7odiluI04AsQVnke5sSz:APA91bHj7xytmfiuBxjm7cvcfADVGgdrs8mhrHPfJvhnVbbj34C0rNrrHHxlBReyRuYYtjO-SczSxYvkdfno5zsvSGuO1UuZkQMMlYOSS4C1I7Urh_YF-1M',
+                'No tasks scheduled for today',
+                'Do you need to reschedule past tasks?',
+                {url: '?rh=tasks&tnav=past'}
+            )
+        }
+    } catch (error) {
+        console.error('Error in schedule:', error)
+    }
+})
+
+schedule.scheduleJob({hour: 21, minute: 30}, async function() { //15:00 UTC = 2:00am/1:00am, 20:30 UTC = 7:30am/6:30am Sydney/Melbourne time
+    //push notification.
+    //Daily nudge to start the timer if there are tasks scheduled.
+    try {
+        const db = await DbConnection.Get()
+        const Tasks = db.collection('tasks')
+
+        const datetime = new Date() //time set from client argument
+        const tzdatetime = datetime //shiftTZ({datetime: datetime, timezoneOffset: -11})
+        const starttime = startOfDay(tzdatetime)
+        const endtime = daylater(tzdatetime) // can retire this later if I want to migrate old DB records.
+
+        let query = {}
+        query.profile = { $in: hard_coded_userids }
+        query.$or = [ //only return if complete not equal to true (or doesn't exist)
+            {complete: null},
+            {complete: false},
+            {complete: {$exists: false}}
+        ]
+
+        query.$and = [ //only return if starttime is between starttime and endtime and snooze is null or snooze is in the past
+            {'starttime': {$gt: starttime}},
+            {'starttime': {$lte: endtime}},
+            {$or: [
+                {snooze: null},
+                {snooze: {$exists: false}},
+                {snooze: {$lt: new Date()}}
+            ]}
+        ]
+        console.log(query)
+        console.log(starttime)
+        console.log(endtime)
+        const tasks = await Tasks.find(query).toArray()
+        console.log(tasks)
+
+        if (tasks.length > 0) {
+            pushNotification(
+                'fJ7odiluI04AsQVnke5sSz:APA91bHj7xytmfiuBxjm7cvcfADVGgdrs8mhrHPfJvhnVbbj34C0rNrrHHxlBReyRuYYtjO-SczSxYvkdfno5zsvSGuO1UuZkQMMlYOSS4C1I7Urh_YF-1M',
+                `${tasks[0].title}`,
+                'Start the timer!',
+                {url:  `?rh=timer&taskid=${tasks[0]._id}`}
+            )
+        }
+    } catch (error) {
+        console.error('Error in schedule:', error)
+    }
+})
+
+schedule.scheduleJob({day: 1, hour: 24, minute: 0}, function() { //15:00 UTC = 2:00am/1:00am, 20:30 UTC = 7:30am/6:30am Sydney/Melbourne time
+    //push notification.
+    //Send a weekly reminder to check in on the top-level wheel.
     pushNotification(
         'fJ7odiluI04AsQVnke5sSz:APA91bHj7xytmfiuBxjm7cvcfADVGgdrs8mhrHPfJvhnVbbj34C0rNrrHHxlBReyRuYYtjO-SczSxYvkdfno5zsvSGuO1UuZkQMMlYOSS4C1I7Urh_YF-1M',
-        'The Early Bird',
-        'Get a head start on today\'s tasks!',
+        'Wheel Check In',
+        'How is your wheel going?',
+        {url: '?rh=wheel'}
+    )
+})
+
+schedule.scheduleJob({hour: 23, minute: 0}, function() { //15:00 UTC = 2:00am/1:00am, 20:30 UTC = 7:30am/6:30am Sydney/Melbourne time
+    pushNotification(
+        'fJ7odiluI04AsQVnke5sSz:APA91bHj7xytmfiuBxjm7cvcfADVGgdrs8mhrHPfJvhnVbbj34C0rNrrHHxlBReyRuYYtjO-SczSxYvkdfno5zsvSGuO1UuZkQMMlYOSS4C1I7Urh_YF-1M',
+        'Check in on your priority list',
+        'How is your wheel going?',
         {url: '?rh=tasks&tnav=priority'}
     )
 })
+
+schedule.scheduleJob({day: 1, hour: 24, minute: 0}, function() { //15:00 UTC = 2:00am/1:00am, 20:30 UTC = 7:30am/6:30am Sydney/Melbourne time
+    //push notification.
+    //Send a weekly reminder to check in on the goals.
+    //This should really check to see if there are any goals. If not, prompt to create a goal.
+    pushNotification(
+        'fJ7odiluI04AsQVnke5sSz:APA91bHj7xytmfiuBxjm7cvcfADVGgdrs8mhrHPfJvhnVbbj34C0rNrrHHxlBReyRuYYtjO-SczSxYvkdfno5zsvSGuO1UuZkQMMlYOSS4C1I7Urh_YF-1M',
+        'Check in on your goals',
+        'These still your high priority goals?',
+        {url: '?rh=goals'}
+    )
+})
+
+//{url: '?rh=tasks&tnav=daytasklist'}
+//{url: '?rh=tasks&tnav=past'}
+//{url: '?rh=tasks&tnav=priority'}
+//{url: '?rh=sources'}
+//{url: '?rh=goals'}
+//{url: '?rh=wheel'}
 
 schedule.scheduleJob({ hour: 15, minute: 0 }, function() { //15:00 UTC = 2:00am/1:00am, 20:30 UTC = 7:30am/6:30am Sydney/Melbourne time
     //email daily stats.
