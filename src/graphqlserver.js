ac8389007d85a46a9b8e5d16a2cd8911706e6830
Daniel Schrader <daniel@lateralproducts.com>
2025-08-24 22:22:45 +1000

    fix: set view only if it exists and returned

--- src/graphqlserver.js
+++ src/graphqlserver.js
@@ -198,7 +198,7 @@ const graphQLServer = createServer({
             // mutate, don't replace
             req.session.user = user
             const view = await getCurrentView(req)
-            await setView(view._id.toString(), req)
+            if (view) await setView(view.id.toString(), req)
             // (optional) persist immediately so Set-Cookie is sent
             await new Promise((r, j) => req.session.save(err => (err ? j(err) : r())))
           } else {
